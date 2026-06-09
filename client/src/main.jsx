e6ff696011cc8a048b822194bfdec415aec9e842
Chirag00555 <chirag.cj555@gmail.com>
2026-06-09 13:37:04 +0530

    test comment

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 //test comment
+//test comment
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
